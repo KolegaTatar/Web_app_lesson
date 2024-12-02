ceb4c053ec608c35a818a759917bd4fd1040c20f
Wiktor <wiktor.tatarynowicz@gmail.com>
2024-12-02 14:04:21 +0100

    update 1.1

---
+++ vite_app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss'
+import '/components/Footer/Footer.scss'
+import Footer from "./components/Footer";
+
+function App() {
+
+  return (
+    <>
+        Hello World
+
+    </>
+  )
+}
+
+export default App
---
+++ vite_app/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import './Footer.scss'
+
+interface FooterProps {
+    copyRight: string
+}
+
+export default function Footer({ copyRight }: FooterProps) {
+    return (
+        <footer>
+            Hejkaaaa
+        </footer>
+    )
+}
