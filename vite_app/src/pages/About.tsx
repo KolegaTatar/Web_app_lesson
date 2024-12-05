8547519322054b7589a02e1a8bd73eefab19e335
Wiktor <wiktor.tatarynowicz@gmail.com>
2024-12-05 09:28:52 +0100

    add 3 websites

---
+++ vite_app/src/pages/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/about.scss";
+
+function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Learn more about our application.</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ vite_app/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/home.scss";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+    <p>This is the main page of the app.</p>
+    </div>
+);
+};
+
+export default Home;
